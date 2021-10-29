8a111c2568ca209f1f49827218460313829c1300
Cheryl Fernandes <73298909+cheryl1908@users.noreply.github.com>
2021-10-29 13:57:57 +0530

    Add files via upload

---
+++ config.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyC4Beu4xLJMuP17lDQ4tFiJj6LzyaiFgjw",
+    authDomain: "spectagram-f5b46.firebaseapp.com",
+    databaseURL: "https://spectagram-f5b46-default-rtdb.firebaseio.com",
+    projectId: "spectagram-f5b46",
+    storageBucket: "spectagram-f5b46.appspot.com",
+    messagingSenderId: "937638008652",
+    appId: "1:937638008652:web:d7cc934b47e9bc4ad1e1e9"
+  };
